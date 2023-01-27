bf58a1f67bc782ed2d461bc4fb17809c608637f7
Angelito10 <92886923+Angelito10@users.noreply.github.com>
2023-01-26 23:32:38 -0600

    Primer commmit

---
+++ main_v4.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route("/html")
+def index():
+    return render_template("index.html")
+
+@app.route("/usuarios")
+def usuarios():
+    return render_template("usuarios.html")
+
+
+if __name__ == "__main__":
+    app.run()
+
